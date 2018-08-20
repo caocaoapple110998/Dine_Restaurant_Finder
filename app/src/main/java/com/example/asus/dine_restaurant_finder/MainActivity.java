fabd3314afa41779d54f1a05e37969e31156ff5b
Lam <lamvo.lvdt98@gmail.com>
2018-08-21 02:53:09 +0700

    menu navigation bar

--- app/src/main/java/com/example/asus/dine_restaurant_finder/MainActivity.java
+++ app/src/main/java/com/example/asus/dine_restaurant_finder/MainActivity.java
@@ -1,27 +1,92 @@
 package com.example.asus.dine_restaurant_finder;
 
+        import android.graphics.drawable.Drawable;
         import android.os.Build;
         import android.os.Bundle;
+        import android.support.annotation.NonNull;
+        import android.support.design.widget.NavigationView;
+        import android.support.v4.widget.DrawerLayout;
+        import android.support.v7.app.ActionBarDrawerToggle;
         import android.support.v7.app.AppCompatActivity;
+        import android.view.MenuItem;
         import android.view.Window;
         import android.view.WindowManager;
+        import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        statusbar();
+        dl = (DrawerLayout) findViewById(R.id.dl);
+        abdt = new ActionBarDrawerToggle(this,dl,R.string.Open, R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
+
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+
+                if (id == R.id.notification)
+                {
+                    Toast.makeText(MainActivity.this,"My Profile", Toast.LENGTH_LONG).show();
+                }
+                if (id == R.id.news)
+                {
+                    Toast.makeText(MainActivity.this,"My news", Toast.LENGTH_LONG).show();
+                }
+                if (id == R.id.bookmarks)
+                {
+                    Toast.makeText(MainActivity.this,"My bookmarks", Toast.LENGTH_LONG).show();
+                }
+                if (id == R.id.offers)
+                {
+                    Toast.makeText(MainActivity.this,"My offers", Toast.LENGTH_LONG).show();
+                }
+                if (id == R.id.orders)
+                {
+                    Toast.makeText(MainActivity.this,"My orders", Toast.LENGTH_LONG).show();
+                }
+                if (id == R.id.payments)
+                {
+                    Toast.makeText(MainActivity.this,"My payments", Toast.LENGTH_LONG).show();
+                }
+                if (id == R.id.comments)
+                {
+                    Toast.makeText(MainActivity.this,"My comments", Toast.LENGTH_LONG).show();
+                }
+                if (id == R.id.profile)
+                {
+                    Toast.makeText(MainActivity.this,"My Profile", Toast.LENGTH_LONG).show();
+                }
+                if (id == R.id.settings)
+                {
+                    Toast.makeText(MainActivity.this,"My settings", Toast.LENGTH_LONG).show();
+                }
+
+
+                return true;
+            }
+        });
+
 
     }
 
-    public void statusbar(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Window w = getWindow(); // in Activity's onCreate() for instance
-            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 }
