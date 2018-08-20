b0b8e6224bc6d04ffcec1cf52dd65edd3c075ef9
Nguyễn Thành Tài <caocaoapple110998@gmail.com>
2018-08-20 19:47:10 +0700

    text

--- app/src/main/java/com/example/asus/dine_restaurant_finder/MainActivity.java
+++ app/src/main/java/com/example/asus/dine_restaurant_finder/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.asus.dine_restaurant_finder;
 
-import android.os.Build;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
-import android.view.WindowManager;
+        import android.os.Build;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.Window;
+        import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
