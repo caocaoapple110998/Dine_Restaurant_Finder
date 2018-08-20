3178f1acff653e11a675f8eabb21740281b2ad83
Lam <lamvo.lvdt98@gmail.com>
2018-08-20 10:32:13 +0700

    login

---
+++ app/src/main/java/com/example/asus/dine_restaurant_finder/Signin_Activity.java
@@ -0,0 +1,13 @@
+package com.example.asus.dine_restaurant_finder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Signin_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin_);
+    }
+}
