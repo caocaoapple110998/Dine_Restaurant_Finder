9d4a407c402b0a0e0d4d99deecd403eaea4c499f
Nguyễn Thành Tài <caocaoapple110998@gmail.com>
2018-08-22 15:11:22 +0700

    list 2

--- app/src/main/java/com/example/asus/dine_restaurant_finder/MainActivity.java
+++ app/src/main/java/com/example/asus/dine_restaurant_finder/MainActivity.java
@@ -1,93 +1,128 @@
 package com.example.asus.dine_restaurant_finder;
 
-        import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
-
-        import com.example.asus.dine_restaurant_finder.Navigation.Bottom_NavigationBar;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private DrawerLayout dl;
-    private ActionBarDrawerToggle abdt;
+//    Toolbar mToolbar;
+//    private DrawerLayout drawerLayout;
+//    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
 
-        dl = (DrawerLayout) findViewById(R.id.dl);
-        abdt = new ActionBarDrawerToggle(this,dl,R.string.Open, R.string.Close);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        abdt.setDrawerIndicatorEnabled(true);
 
-        dl.addDrawerListener(abdt);
-        abdt.syncState();
+        /////////////////////////////////////////////////////////////////////////
+//        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+//
+//        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.Open, R.string.Close);
+//        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+//        actionBarDrawerToggle.syncState();
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//
+//        NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
+//
+//        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//
+//                int id = item.getItemId();
+//
+//                if (id == R.id.notification)
+//                {
+//                    Toast.makeText(MainActivity.this,"My Profile", Toast.LENGTH_LONG).show();
+//                }
+//                if (id == R.id.news)
+//                {
+//                    Toast.makeText(MainActivity.this,"My news", Toast.LENGTH_LONG).show();
+//                }
+//                if (id == R.id.bookmarks)
+//                {
+//                    Toast.makeText(MainActivity.this,"My bookmarks", Toast.LENGTH_LONG).show();
+//                }
+//                if (id == R.id.offers)
+//                {
+//                    Toast.makeText(MainActivity.this,"My offers", Toast.LENGTH_LONG).show();
+//                }
+//                if (id == R.id.orders)
+//                {
+//                    Toast.makeText(MainActivity.this,"My orders", Toast.LENGTH_LONG).show();
+//                }
+//                if (id == R.id.payments)
+//                {
+//                    Toast.makeText(MainActivity.this,"My payments", Toast.LENGTH_LONG).show();
+//                }
+//                if (id == R.id.comments)
+//                {
+//                    Toast.makeText(MainActivity.this,"My comments", Toast.LENGTH_LONG).show();
+//                }
+//                if (id == R.id.profile)
+//                {
+//                    Toast.makeText(MainActivity.this,"My Profile", Toast.LENGTH_LONG).show();
+//                }
+//                if (id == R.id.settings)
+//                {
+//                    Toast.makeText(MainActivity.this,"My settings", Toast.LENGTH_LONG).show();
+//                }
+//
+//
+//                return true;
+//            }
+//        });
+//
+//
+//    }
+//
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        return actionBarDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+//    }
+}
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    private void CreateToolbar() {
 
-        NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
+    }
 
-        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu) {
+        getMenuInflater().inflate( R.menu.menu_search_toolbar, menu);
+
+        MenuItem myActionMenuItem = menu.findItem( R.id.action_search);
+        final SearchView searchView = (SearchView) myActionMenuItem.getActionView();
+        searchView.setQueryHint("Search by landmark");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
 
-                int id = item.getItemId();
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (TextUtils.isEmpty(newText)) {
 
-                if (id == R.id.notification)
-                {
-                    Toast.makeText(MainActivity.this,"My Profile", Toast.LENGTH_LONG).show();
+                } else {
                 }
-                if (id == R.id.news)
-                {
-                    Toast.makeText(MainActivity.this,"My news", Toast.LENGTH_LONG).show();
-                }
-                if (id == R.id.bookmarks)
-                {
-                    Toast.makeText(MainActivity.this,"My bookmarks", Toast.LENGTH_LONG).show();
-                }
-                if (id == R.id.offers)
-                {
-                    Toast.makeText(MainActivity.this,"My offers", Toast.LENGTH_LONG).show();
-                }
-                if (id == R.id.orders)
-                {
-                    Toast.makeText(MainActivity.this,"My orders", Toast.LENGTH_LONG).show();
-                }
-                if (id == R.id.payments)
-                {
-                    Toast.makeText(MainActivity.this,"My payments", Toast.LENGTH_LONG).show();
-                }
-                if (id == R.id.comments)
-                {
-                    Toast.makeText(MainActivity.this,"My comments", Toast.LENGTH_LONG).show();
-                }
-                if (id == R.id.profile)
-                {
-                    Toast.makeText(MainActivity.this,"My Profile", Toast.LENGTH_LONG).show();
-                }
-                if (id == R.id.settings)
-                {
-                    Toast.makeText(MainActivity.this,"My settings", Toast.LENGTH_LONG).show();
-                }
-
-
                 return true;
             }
         });
 
-
+        return true;
     }
 
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/com/example/asus/dine_restaurant_finder/Navigation/Navigation_Menu.java
+++
@@ -1,8 +0,0 @@
-package com.example.asus.dine_restaurant_finder.Navigation;
-
-/**
- * Created by Administrator on 22/08/2018.
- */
-
-public class Navigation_Menu {
-}
