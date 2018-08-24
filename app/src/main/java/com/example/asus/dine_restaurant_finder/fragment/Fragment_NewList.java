cc74d70ac6a267c18592899128c882d2921b2301
Lam <lamvo.lvdt98@gmail.com>
2018-08-24 14:27:54 +0700

    update navigation

--- app/src/main/java/com/example/asus/dine_restaurant_finder/fragment/Fragment_NewList.java
+++ app/src/main/java/com/example/asus/dine_restaurant_finder/fragment/Fragment_NewList.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -60,11 +61,12 @@ public class Fragment_NewList extends Fragment {
 
         drawerLayout = (DrawerLayout )view.findViewById(R.id.drawer_layout_newlist);
 
-        actionBarDrawerToggle = new ActionBarDrawerToggle(activity, drawerLayout, R.string.Open, R.string.Close);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, R.string.Open, R.string.Close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
 
         activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        activity.getSupportActionBar().setHomeButtonEnabled(true);
 
         NavigationView nav_view = (NavigationView) view.findViewById(R.id.nav_view_newlist);
 
@@ -153,9 +155,17 @@ public class Fragment_NewList extends Fragment {
     }
 
 
-        @Override
+//        @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        return actionBarDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+//    }
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return actionBarDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
-    
 }
